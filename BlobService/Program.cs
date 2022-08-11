535ab52ea35c86ca27a4faaf24124b2a66162d39
Alex Wilber <AlexW@cqons.onmicrosoft.com>
2022-08-11 20:11:14 +0530

    Add project files.

---
+++ BlobService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Blobs;
+using System;
+
+// Note: actual namespace depends on the project name.
+    public class Program
+    {
+        private static string containerName = "data";
+        private static string connectionString = "";
+        
+
+        static void Main(string[] args)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+             blobServiceClient.CreateBlobContainer(containerName);
+
+
+            Console.WriteLine("Container Created");
+
+             Console.ReadKey();
+        }
+    }
